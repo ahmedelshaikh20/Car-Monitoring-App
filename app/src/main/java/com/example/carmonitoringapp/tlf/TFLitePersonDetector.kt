d18e9cce67b17e00fc564956856f6f11cc9009af
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-07-07 01:46:39 +0200

    Creating TFLITE DETECTOR to handle person detection

---
+++ app/src/main/java/com/example/carmonitoringapp/tlf/TFLitePersonDetector.kt
@@ -0,0 +1,49 @@
+package com.example.carmonitoringapp.tlf
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import com.example.carmonitoringapp.data.model.CustomBoundingBox
+import com.example.carmonitoringapp.data.model.PersonDetectionEvent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.Rot90Op
+
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
+import javax.inject.Inject
+
+class TFLitePersonDetector @Inject constructor(
+  @ApplicationContext context: Context,
+  val objectDetector: ObjectDetector
+) {
+
+
+  fun detectPersons(
+    bitmap: Bitmap,
+    rotation: Int,
+    timestamp: String
+  ): List<PersonDetectionEvent> {
+    val imageProcessor = ImageProcessor.Builder()
+      .add(Rot90Op(-rotation / 90))
+      .build()
+
+    val tensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+    val detections = objectDetector?.detect(tensorImage) ?: emptyList()
+    Log.d("TFLitePersonDetector", "Detected ${detections} persons")
+    return detections.filter { it.categories[0].label == "person" }.mapIndexed { index, detection ->
+      val box = detection.boundingBox
+      PersonDetectionEvent(
+        timestamp = timestamp,
+        personId = "tflite_person_$index",
+        boundingBox = CustomBoundingBox(
+          x = box.left.toInt(),
+          y = box.top.toInt(),
+          width = box.width().toInt(),
+          height = box.height().toInt()
+        ),
+        detectedAction = "unknown"
+      )
+    }
+  }
+}
