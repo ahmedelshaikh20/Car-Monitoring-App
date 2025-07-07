bd22e3855e0c7b46e21538e309e6b65b9ef817f8
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-07-07 04:02:42 +0200

    Adding NetweorkModule to able to inject HttpClient using Ktor

--- app/src/main/java/com/example/carmonitoringapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/carmonitoringapp/di/NetworkModule.kt
@@ -1,2 +1,26 @@
 package com.example.carmonitoringapp.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+  @Provides
+  @Singleton
+  fun provideHttpClient(): HttpClient {
+    return HttpClient(OkHttp) {
+      install(ContentNegotiation) {
+        json()
+      }
+    }
+  }
+}
